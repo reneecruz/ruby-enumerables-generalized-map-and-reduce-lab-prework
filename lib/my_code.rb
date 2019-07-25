1c06e0cd454c4172c67adcdc09934071103c971c
Renée Cruz <reneebasscruz@gmail.com>
2019-07-25 17:21:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,7 +16,7 @@ def reduce(source_array, starting_point = 0)
   memo = 0
   
   while counter < source_array.length do 
-    sum = yield(source_array[counter])
+    sum = yield(source_array[counter]) 
     ends
     counter += 1
   end
